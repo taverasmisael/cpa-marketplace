7bacf8ca22993f22e9e85fe595258aef5a1e1f1e
Misael Taveras <macrot29@gmail.com>
2017-11-07 14:22:17 -0400

    refact: simplify and optimize sorting

--- src/utilities/offerSort.js
+++ src/utilities/offerSort.js
@@ -1,19 +1,3 @@
 import { descend, ascend, prop, sort } from 'ramda'
-
-export const name = sort(ascend(prop('name')))
-export const nameDesc = sort(descend(prop('name')))
-
-export const payout = sort(ascend(prop('payout')))
-export const payoutDesc = sort(descend(prop('payout')))
-
-export const revenue = sort(ascend(prop('revenue')))
-export const revenueDesc = sort(descend(prop('revenue')))
-
-export const clicks = sort(ascend(prop('clicks')))
-export const clicksDesc = sort(descend(prop('clicks')))
-
-export const cr = sort(ascend(prop('cr')))
-export const crDesc = sort(descend(prop('cr')))
-
-export const erpm = sort(ascend(prop('erpm')))
-export const erpmDesc = sort(descend(prop('erpm')))
+export const SortBy = key => sort(ascend(prop(key)))
+export const SortDescBy = key => sort(descend(prop(key)))
